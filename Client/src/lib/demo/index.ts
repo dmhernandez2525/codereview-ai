d5cd0b52f394e0f8fa6cd192e3e33e4fa410fdd8
Daniel Hernandez <danielmark252525@GMAIL.COM>
2026-02-01 10:33:23 -0800

    feat: add demo mode for code review (#25)

---
+++ Client/src/lib/demo/index.ts
@@ -0,0 +1,10 @@
+export { DemoProvider, useDemo, useDemoOptional } from './context';
+export {
+  demoUser,
+  demoRepositories,
+  demoReviews,
+  demoFullReviews,
+  demoDashboardStats,
+  demoRecentReviews,
+  getDemoReviewById,
+} from './data';
